deb107635aee184fb2f894f4f08b81357f3734ef
Aircity <aircity@drrr.us>
2019-01-20 14:41:24 +0800

    feat:subtify

---
+++ bin/subtify.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+const globby = require('globby');
+
+execSync("export http_proxy='http://127.0.0.1:1080'")
+execSync("export https_proxy='http://127.0.0.1:1080'")
+
+const files  = globby.sync(['*.mp4'])
+
+for (file of files)
+{
+  execSync(`autosub ${file}`,  {stdio: 'inherit'})
+}

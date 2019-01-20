c566ff0642a10db0098d4f834dd47d054aeb1634
Aircity <aircity@drrr.us>
2019-01-20 16:43:22 +0800

    fix:filename not match

--- bin/subtify.js
+++ bin/subtify.js
@@ -6,9 +6,9 @@ const globby = require('globby');
 execSync("export http_proxy='http://127.0.0.1:1080'")
 execSync("export https_proxy='http://127.0.0.1:1080'")
 
-const files  = globby.sync(['*.mp4'])
+const files  = globby.sync(['*.mp4','*.mkv'])
 
 for (file of files)
 {
-  execSync(`autosub ${file}`,  {stdio: 'inherit'})
+  execSync(`autosub '${file}'`,  {stdio: 'inherit'})
 }
